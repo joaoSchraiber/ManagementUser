64b2a329f1c8385e2b43e80f9f64f3455a1662ed
João Schraiber <joaoschraiber21@gmail.com>
2026-06-10 21:00:19 -0300

    feat: Adiciona DTOs de Usuário e Perfil

--- ManagementUser/Program.cs
+++ ManagementUser/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using ManagementUser.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configuração do MySQL com Pomelo
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    )
+);
 
 var app = builder.Build();
 
@@ -17,9 +28,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
